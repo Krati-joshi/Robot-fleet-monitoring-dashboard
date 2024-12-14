75d5491136eda1288caa69f385291cc26994ace6
Krati-joshi <156800478+Krati-joshi@users.noreply.github.com>
2024-12-15 02:54:03 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -10,11 +10,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://kratijoshi.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class Robot(BaseModel):
     robot_id: str
